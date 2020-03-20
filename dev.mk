339cc631fd3037dec08fc3253bb3290ef5c897b9
tamakiii <tamakiii@users.noreply.github.com>
2020-03-20 12:33:11 +0000

    fix nodemon exit code

--- dev.mk
+++ dev.mk
@@ -10,7 +10,7 @@ node_modules:
 	npm install --dev
 
 server:
-	npx --no-install nodemon -w src --exec "make server"
+	npx --no-install nodemon -w src --exec "make server || exit 1"
 
 clean:
 	rm -rf node_modules
